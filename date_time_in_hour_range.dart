d61db3b458b9cc3518df42d7b88961d68c2b0543
Иван Мисько <96063296+VeesileNavi@users.noreply.github.com>
2023-03-15 23:24:40 +0300

    Add files via upload

---
+++ date_time_in_hour_range.dart
@@ -0,0 +1,38 @@
+List<DateTime> getDateTimeInHourRange(DateTime selectedDate, List<int> hourRange) {
+  assert(hours.length<=2,'There is must be only 2 members: period start an period end');
+  assert(hours.first<hours.last, 'Period end cannot be less than period start');
+
+  final firstTimeMinutes = hourRange.first<selectedDate.hour
+  ?selectedDate.minute
+  :0;
+  
+  final firstTime = DateTime(
+        selectedDate.year, 
+        selectedDate.month, 
+        selectedDate.day, 
+        math.max(hourRange.first, selectedDate.hour),
+        firstTimeMinutes,
+    );
+  
+  
+  final lastTime = DateTime(
+        selectedDate.year, 
+        selectedDate.month, 
+        selectedDate.day, 
+        hourRange.last,
+        );
+
+  
+  final startTime = firstTime.add(
+          Duration(minutes: 30 - firstTime.minute % 30,),
+      );
+      
+  final List<DateTime> output = [];
+  
+    for (DateTime time = startTime; time.isBefore(lastTime) || time.isAtSameMomentAs(lastTime);
+    time = time.add(const Duration(minutes: 30))) {
+      output.add(time);
+    }
+  
+    return output;
+  }
